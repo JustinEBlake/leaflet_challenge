256c58711d921024efd5bec0c5b231a272f4b4c4
JustinEBlake <116922273+JustinEBlake@users.noreply.github.com>
2024-01-28 19:53:22 -0600

    added legend

--- main/static/js/logic.js
+++ main/static/js/logic.js
@@ -1,4 +1,4 @@
-// Get url for earthquakes in past 7 days
+// Get url for significant earthquakes in the past month
 url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
 //Console log data
@@ -49,11 +49,14 @@ d3.json(url).then((data)=>{
 	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	ext: 'png'
     }).addTo(myMap);
+
+    // Call createLegend function
+    createLegend(myMap);
 })
 
 // Define a function to determine the marker size based on the magnitude
 function markerSize(mag) {
-    return Math.sqrt(mag) *100000 ;
+    return (mag/2) *105000 ;
   }
 
 
@@ -66,3 +69,30 @@ function getColor(quake_depth) {
            quake_depth > 29 ? '#F7DB11' :
                           '#A3F500';
   }
+
+// Define a function to create a legend
+function createLegend(myMap) {
+    let legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function (map) {
+        let div = L.DomUtil.create('div', 'info legend');
+        div.style.backgroundColor = 'white'; 
+        let grades = [-10, 10, 30, 50, 70, 90]; 
+        let labels = [];
+
+        // Loop through the depth grades and generate a label with a colored square for each grade
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background-color:' + getColor(grades[i] + 1) + '"></i> ' +
+                (grades[i + 1] ? grades[i] + '&ndash;' + grades[i + 1] + '<br>' : '+' + grades[i] + '<br>');
+        }
+
+        return div;
+    };
+
+    legend.addTo(myMap);
+}
+
+
+
+
